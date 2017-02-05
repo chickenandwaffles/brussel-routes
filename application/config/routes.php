df1c66e8f9096638a55f4b630cadc8cc3c160b46
kevintieu <kevin.tieu@hotmail.com>
2017-02-05 15:57:04 -0800

    closes #11

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'Hogwarts';
-$route['404_override'] = '';
+$route['404_override'] = 'Hogwarts/random';
 $route['translate_uri_dashes'] = FALSE;
 $route['sleep'] = 'first/zzz';
 $route['lock/em/up'] = "Hogwarts/shucks";
